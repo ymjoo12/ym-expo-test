0dbbc32af0832b38d6d1d17832df2df1e33b7f1f
ymjoo12 <ymjoo12@naver.com>
2021-06-18 17:54:55 +0900

    add: firebase

--- App.tsx
+++ App.tsx
@@ -7,6 +7,21 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAJeWco-ULuYeO6zhAgxirhGQphpK-6CJI",
+  authDomain: "test-ym-2b413.firebaseapp.com",
+  projectId: "test-ym-2b413",
+  storageBucket: "test-ym-2b413.appspot.com",
+  messagingSenderId: "224970998165",
+  appId: "1:224970998165:web:7d19cb2a564605d0c2b69c",
+  measurementId: "G-DMD36MZYPK"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
